10387f36a8a02acbab195d7d974b8c234d9a42e9
Clark Williams <theclarkwilliams@gmail.com>
2019-07-17 14:44:03 -0600

    have form validation minus TOS working

--- user_onboarding/src/Components/Form.js
+++ user_onboarding/src/Components/Form.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-const UserForm = () => {
+function UserForm({ values, errors, touched }) {
     return (
         <Form className="user-form" >
-            <Field type="text" name="name" placeholder="name"  className="field" />
-            <Field type="email" name="email" placeholder="email"  className="field" />
-            <Field type="password" name="password" placeholder="password"  className="field" />
-            <label htmlFor="tos">I accept Terms of Service</label>
-            <Field type="checkbox" name="tos" id="tos" className="field" />
+            <div className="field">
+                <label htmlFor="name">Name: </label>
+                <Field type="text" name="name" id="name" placeholder="name" />
+                {touched.name && errors.name && <p>{errors.name}</p>}
+            </div>
+            <div className="field">
+                <label htmlFor="email">Email: </label>
+                <Field type="email" name="email" id="email" placeholder="email" />
+                {touched.email && errors.email && <p>{errors.email}</p>}
+            </div>
+            <div className="field">
+                <label htmlFor="password">Password: </label>
+                <Field type="password" name="password" id="password" placeholder="password" />
+                {touched.password && errors.password && <p>{errors.password}</p>}
+            </div>
+            <div className="field">
+                <label htmlFor="tos">I accept Terms of Service</label>
+                <Field type="checkbox" name="tos" id="tos" checked={values.tos}/>
+            </div>
             <button>Submit</button>
         </Form>
     );
@@ -16,13 +31,27 @@ const UserForm = () => {
 
 const FormikUserForm = withFormik({
     
-    mapPropsToValues({ name, email, password }) {
+    mapPropsToValues({ name, email, password, tos }) {
         return {
             name: name || "",
             email: email || "",
-            password: password || ""
+            password: password || "",
+            tos : tos || false
         };
     },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+            // .name("Name not valid")
+            .min(2, "Name must be 2 characters or longer")
+            .required("Name is required"),
+        email: Yup.string()
+            .email("Email not valid")
+            .required("Email is required"),
+        password: Yup.string()
+            .min(8, "Password must be 8 characters or longer")
+            .required("Password is required")
+    }),
 
     handleSubmit(values) {
         console.log(values);
